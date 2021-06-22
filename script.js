a073f36cc070b6a47f203ffdbfb6a1a344ae87af
sean goudie <sgoudie77@hotmail.com>
2021-06-22 07:24:15 -0700

    Added countUp, refresh button, and flight display

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+import { CountUp } from './countUp.min.js';
+
 // Set the starting map position and zoom ([lat, lon], zoom-level)
 var map = L.map('map').setView([58.63121664342478, -93.77929687500001], 4);
 
 // Add a default map from Map Tiler
-streets = L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=CFUEyunMBd2JLEvHOg9L', {
+const streets = L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=CFUEyunMBd2JLEvHOg9L', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     crossOrigin: true
 }).addTo(map);
@@ -32,7 +34,6 @@ map.on('click', function(e){
 async function flightsData() {
   const apiResponse = await fetch('https://opensky-network.org/api/states/all');
   const result = await apiResponse.json();
-  console.log(result);
   return await result.states;
 }
 
@@ -40,32 +41,38 @@ async function flightsCount(data) {
   const counts = await data.length;
   console.log(counts);
   document.getElementById("loader").style.display = 'none';
+
+  // Delay showing the refresh button until the page loads 
+  setTimeout(showLatest, 5000);
+  function showLatest() {
+    document.getElementById('latest').style.opacity = 1;
+  }
+
   return await counts;
 }
 
 async function displayTotalFlights(counts) {
   //Set variable for total flights
   const totalResults = await counts;
-  //Display number of total flights using a div id
-  document.getElementById('total').innerHTML = await totalResults;
+  //countUp display total number animation
+  const options = {
+    startVal: 0,
+    decimalPlaces: 0,
+    duration: 8,
+    useEasing: true,
+    separator: ',',
+  };
+  
+  const countUp = new CountUp('total', await totalResults, options);
+  console.log(countUp);
+  if (!countUp.error) {
+    countUp.start();
+  } else {
+    console.error(countUp.error);
+  }
 }
 
-
-
-// countUp display total number animation (id, starting point, ending point, decimal, duration)
-// var options = {
-//   useEasing: true,
-//   separator: ';'
-// };
-// var countUp = new CountUp('#total', 0, counts, 0, 5, options);
-// if (!countUp.error) {
-//   countUp.start(document.getElementById('total').innerHTML = await totalResults);
-// } else {
-//   console.error(countUp.error);
-// }
-
-
-
+//document.getElementById('total').innerHTML = await totalResults;
 //displayTotalFlights(flightsCount(flightsData()));
 
 //Set the icon for each flight reporting data
@@ -74,14 +81,22 @@ var aircraftTracked = L.icon ({
   iconSize: [18, 12]
 });
 
+// Set the icon for mouseover
+var aircraftHover = L.icon ({
+  iconUrl: 'ac-blue.png',
+  iconSize: [18, 12] 
+});
+
+
 async function displayMarkers(data) {
   //Loop through the array and set an icon for each flight using the lat and lon coords of each object
   let noCoordinates = 0;
   for (let i = 0; i < data.length; i++) {
     if (data[i][5] != null && data[i][6] != null) {
-      console.log(data[i][6], data[i][5])
       let displayedFlight = new L.marker([data[i][6],data[i][5]], {icon: aircraftTracked}).addTo(map)
-      .bindPopup(data[i][1],data[i][2],data[i][7]);
+      .bindPopup(`<p>Flight Number: ${data[i][1]}</p>
+                  <p>Origin Country: ${data[i][2]}</p>
+                  <p>Altitude: ${ Math.floor(data[i][7] * 3.28084) } feet</p>`);
     } else {
       noCoordinates++;
     }
