3f67b163b6c0ea7a3a6f994e8397000a2a020a7e
sean goudie <sgoudie77@hotmail.com>
2021-07-17 10:56:27 -0700

    Flight query code second attmpt

--- script2.js
+++ script2.js
@@ -1,100 +1,106 @@
-const api = {
-    key: "046713ea22076c597022482282a4f711",
-    base: "https://api.aviationstack.com/v1/flights"
-}
+// Set variables for api
+//let url = 'http://api.aviationstack.com/v1/flights',
+  //  qString = '?access_key=' + '046713ea22076c597022482282a4f711'
 
-// Set variable for the flights searchbox input
-const searchbox = document.querySelector('.search-box-flights');
-searchbox.addEventListener('keypress', setQueryFlights);
+// Set variable for the flight searchbox input
+//const searchboxFlight = document.querySelector('.search-box-flights');
+//searchboxFlight.addEventListener('keypress', getFlightQuery);
 
 // Search button linked to flights search bar
-document.querySelector('.search-box-flights button').addEventListener('click', function () {
-    if (searchbox.value == data.flight[1] && data == "") {
-        return airlineName.innerText = `<p>no result found</p>`;
-    } else if (searchbox.value == data.flight[1]) {
-        getResults();
-    }
-});
+// document.querySelector('.search-box-flights button').addEventListener('click', function () {
+//     getFlightResults();
+// });
 
 // Get results when enter key is pressed in flights search
-function setQueryFlights(e) {
-    if (e.keyCode == 13 && data == "")  {
-        return airlineName.innerText = `<p>no result found</p>`;
-    } else if (e.keyCode == 13 && searchbox.value == data.flight[1]) {
-        getResults();
-    }
-}
+// function getFlightQuery(e){
+//     if (e.keyCode == 13) {
+//         getFlightResults();
+//     }
+// }
 
 // Fetch request to the API to get the data
-function getResults (query) {
-    fetch(`${api.base}?access_key=${api.key}`).then(data => {
-        return data.json();
-    }).then(displayResultsFlights);
-}
-
-// Set variables to display the flight information results
-function displayResultsFlights (data) {
-    let airlineName = document.querySelector('#airline-title');
-    airlineName.innerText = `${data.airline[0]}`;
-
-    let flightNumber = document.querySelector('#flight-number');
-    flightNumber.innerText = `${data.flight[1]}`
-
-    // departure info section
-    let departureAirport = document.querySelector('#dep-airport');
-    departureAirport.innerText = `${data.departure[0]}`;
-
-    let departureTerminal = document.querySelector('#dep-terminal');
-    departureTerminal.innerText = `${data.departure[4]}`;
-
-    let departureGate = document.querySelector('#dep-gate');
-    departureGate.innerText = `${data.departure[5]}`;
-
-    let departureStatus = document.querySelector('#status');
-    departureStatus.innerText = `${data.flight_status}`;
-
-    let scheduledDeparture = document.querySelector('#dep-time-sch');
-    scheduledDeparture.innerText = `${data.departure[7]}`;
-
-    let actualDeparture = document.querySelector('#dep-time-act');
-    actualDeparture.innerText = `${data.departure[9]}`;
-
-    let departureDelay = document.querySelector('#dep-delay');
-    departureDelay.innerText = `${data.departure[6]}minutes`;
-
-    // arrival info section
-    let arrivalAirport = document.querySelector('#arr-airport');
-    arrivalAirport.innerText = `${data.arrival[0]}`;
-
-    let arrivalTerminal = document.querySelector('#arr-terminal');
-    arrivalTerminal.innerText = `${data.arrival[4]}`;
-
-    let arrivalGate = document.querySelector('#arr-gate');
-    arrivalGate.innerText = `${data.arrival[5]}`;
-
-    let arrivalBag = document.querySelector('#bag');
-    arrivalBag.innerText = `${data.arrival[6]}`;
-
-    let scheduledArrival = document.querySelector('#arr-time-sch');
-    scheduledArrival.innerText = `${data.arrival[8]}`;
-
-    let estimatedArrival = document.querySelector('#arr-time-est');
-    estimatedArrival.innerText = `${data.arrival[9]}`;
-
-    let arrivalDelay = document.querySelector('#arr-delay');
-    arrivalDelay.innerText = `${data.arrival[7]}minutes`;
-}
+// function getFlightResults() {
+//     fetch(url + qString)
+//         .then(function(resp) {
+//             return resp.json();
+//         })
+//         .then(function(data) {
+//             console.log(data); 
+//             for (var i = 0; i < arrayLength; i++) {
+//                 if (find(searchboxFlight.value == -1)) {
+//                     alert('No flight information found');
+//                 } else if (find(searchboxFlight.value)) {
+//                     displayFlightResults();
+//                 }
+//             }
+//         })
+// }
 
 // Convert date and time string to new format for display
-var dateFormat = new Date(`${data.departure[7]}`).toLocaleDateString("sq-AL",{ 
-    weekday: 'short',
-    year: 'numeric', 
-    month: 'short', 
-    day: '2-digit', 
-    hour: '2-digit', 
-    minute: '2-digit', 
-    timeZoneName: 'short' 
-});
+// var dateFormat = new Date(`${data.data[0].departure.scheduled, data.data[0].departure.estimated, data.data[0].arrival.schedule, data.data[0].arrival.estimated}`).toLocaleDateString("sq-AL",{ 
+//     weekday: 'short',
+//     year: 'numeric', 
+//     month: 'short', 
+//     day: '2-digit', 
+//     hour: '2-digit', 
+//     minute: '2-digit', 
+//     timeZoneName: 'short' 
+// });
+
+// Set variables to display the flight information results
+// function displayFlightResults(data) {
+//     let airlineName = document.getElementById('airline-title');
+//     airlineName.innerHTML = `${data.data.airline.name}`;
+
+//     let flightNumber = document.getElementById('flight-number');
+//     flightNumber.innerHTML = `${data.data.flight.iata}`;
+
+    // departure info section
+    // let departureAirport = document.querySelector('#dep-airport');
+    // departureAirport.innerHTML = `${data.data.departure.airport}`;
+
+    // let departureTerminal = document.querySelector('#dep-terminal');
+    // departureTerminal.innerHTML = `${data.data.departure.terminal}`;
+
+    // let departureGate = document.querySelector('#dep-gate');
+    // departureGate.innerHTML = `${data.data.departure.gate}`;
+
+    // let departureStatus = document.querySelector('#status');
+    // departureStatus.innerHTML = `${data.data.flight_status}`;
+
+    // let scheduledDeparture = document.querySelector('#dep-time-sch');
+    // scheduledDeparture.innerHTML = `${data.data.departure.scheduled}`;
+
+    // let estimatedDeparture = document.querySelector('#dep-time-est');
+    // estimatedDeparture.innerHTML = `${data.data.departure.estimated}`;
+
+    // let departureDelay = document.querySelector('#dep-delay');
+    // departureDelay.innerHTML = `${data.data.departure.delay}minutes`;
+
+    // arrival info section
+//     let arrivalAirport = document.querySelector('#arr-airport');
+//     arrivalAirport.innerHTML = `${data.data.arrival.airport}`;
+
+//     let arrivalTerminal = document.querySelector('#arr-terminal');
+//     arrivalTerminal.innerHTML = `${data.data.arrival.terminal}`;
+
+//     let arrivalGate = document.querySelector('#arr-gate');
+//     arrivalGate.innerHTML = `${data.data.arrival.gate}`;
+
+//     let arrivalBag = document.querySelector('#bag');
+//     arrivalBag.innerHTML = `${data.data.arrival.baggage}`;
+
+//     let scheduledArrival = document.querySelector('#arr-time-sch');
+//     scheduledArrival.innerHTML = `${data.data.arrival.schedule}`;
+
+//     let estimatedArrival = document.querySelector('#arr-time-est');
+//     estimatedArrival.innerHTML = `${data.data.arrival.estimated}`;
+
+//     let arrivalDelay = document.querySelector('#arr-delay');
+//     arrivalDelay.innerHTML = `${data.data.arrival.delay}minutes`;
+// }
+
+// displayFlightResults();
 
 // hamburger menu toggle
 function toggleMenu(){
@@ -103,3 +109,88 @@ function toggleMenu(){
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+
+
+
+
+
+// Search button linked to flights search bar
+//document.querySelector('.search-box-flights button').addEventListener('click', function () {
+    // for (var i = 0; i < arrayLength; i++) {
+    //     if (e.keyCode == 13 && find(searchboxFlight.value) == -1) {
+    //         alert('No flight information found');
+    //         return document.getElementById('airline-title').innerHTML = 'no flight data found';
+    //     } else if (find(searchboxFlight.value)) {
+    //         getFlightResults();
+    //     }
+    // } 
+//});
+
+// Get results when enter key is pressed in flights search
+// function getFlightQuery(e){
+//     for (var i = 0; i < arrayLength; i++) {
+//         if (e.keyCode == 13 && find(searchboxFlight.value) == -1) {
+//             alert('No flight information found');
+//             return document.getElementById('airline-title').innerHTML = 'no flight data found';
+//         } else if (find(searchboxFlight.value)) {
+//             getFlightResults();
+//         }
+//     }
+// }
+
+// Search button linked to flights search bar
+// document.querySelector('.search-box-flights button').addEventListener('click', function () {
+//     if (searchboxFlight.value == data.data[0].flight.iata && data.data[0].flight.iata == null) {
+//         return document.getElementById('airline-title').innerHTML = 'no flight data found';
+//     } else if (searchboxFlight.value == data.data[0].flight.iata) {
+//         getFlightResults();
+//     }
+// });
+
+// const api = {
+//     key: "046713ea22076c597022482282a4f711",
+//     base: "http://api.aviationstack.com/v1/flights"
+// }
+
+// Get results when enter key is pressed in flights search
+// function getFlightQuery(e) {
+//     if (e.keyCode == 13 && searchboxFlight.value == data.data[0].flight.iata && data.data[0].flight.iata == null) {
+//         return document.getElementById('airline-title').innerHTML = 'no flight data found';
+//     } else if (searchboxFlight.value == data.data[0].flight.iata) {
+//         getFlightResults();
+//     }
+// }
+
+// Get results when enter key is pressed in flights search
+// function setQueryFlights(e) {
+//     if (e.keyCode == 13 && data == "")  {
+//         return airlineName.innerText = `<p>no result found</p>`;
+//     } else if (e.keyCode == 13 && searchbox.value == data.flight[1]) {
+//         getResults();
+//     }
+// }
+
+// Fetch request to the API to get the data
+// function getResults (query) {
+//     fetch(`${api.base}?access_key=${api.key}`).then(data => {
+//         return data.json();
+//     }).then(displayResultsFlights);
+// }
+
+let flight = {
+    qString: '?access_key=' + '046713ea22076c597022482282a4f711',
+    fetchFlight: function() {
+        fetch(
+            'http://api.aviationstack.com/v1/flights' + this.qString
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayFlight(data));
+    },
+    displayFlight: function(data) {
+        const { name } = data;
+        const { iata } = data.flight;
+        console.log(name,iata);
+        document.getElementById('airline-title').innerText = name;
+        document.getElementById('flight-number').innerText = iata;
+    }
+};
